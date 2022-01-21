212b96dbc043c3414ae4659a77d8373b2450b7fa
adolivaresl <domioli@gmail.com>
2022-01-21 12:51:45 -0300

    funcion decode

--- src/cipher.js
+++ src/cipher.js
@@ -9,6 +9,14 @@ const encodeChar = (offset, char) => {
   return String.fromCharCode((newCode % ALPHABET_LENGTH) + FIRST_LETTER_CODE);
 };
 
+const decodeChar = (offset, char) => {
+  const charCode = char.charCodeAt(0);
+  const zeroBasedCode = charCode - FIRST_LETTER_CODE;
+  const newCode = zeroBasedCode - offset;
+
+  return String.fromCharCode((newCode % ALPHABET_LENGTH) + FIRST_LETTER_CODE);
+};
+
 const cipher = {
   encode: (offset, msg) => {
     console.log("Esto de abajo es un console log:");
@@ -25,6 +33,16 @@ const cipher = {
 
     return encodedMsg.join("");
   },
+  decode: (offset, msg) => {
+    if (typeof offset !== 'number' || typeof msg !== 'string') {
+      throw new TypeError("Wrong argument types");
+    }
+    const decodedMsg = [];
+    for (const char of msg ){
+      decodedMsg.push(decodeChar(offset, char));
+    }
+    return decodedMsg.join("");
+  }
 };
 
 export default cipher;
